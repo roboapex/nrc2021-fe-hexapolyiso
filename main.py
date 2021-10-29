3054bee1460f5dc2df897e35b5a72dfeb4d94666
Jerick Seng <64419693+jer123se12@users.noreply.github.com>
2021-10-29 16:32:01 +0800

    Specify commit message

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import cv2
 import numpy
 
+wid=320
+hei=240
+
+
+
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH,320)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH,wid)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT,hei)
 
 
 #Value for red (To be changed)
@@ -21,6 +26,8 @@ upper_green = numpy.array([90, 255, 255])
 
 minsize=2000
 while (1):
+    redbiggest = 0
+    xcoor=None
     _, frame = cap.read()
     blurred = cv2.medianBlur(frame, 15)
 
@@ -41,33 +48,38 @@ while (1):
     
     contours2, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
 
-    try:
+    if len(contours)>0:
         c= max(contours, key = cv2.contourArea)
         if cv2.contourArea(c)>=minsize:
             cv2.drawContours(frame, c, -1, (0,255,0), 3)
             x,y,w,h =cv2.boundingRect(c)
             cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
             cv2.line(frame,(int(x+(w/2)),0),(int(x+(w/2)),len(blurred)),(0,255,0),1)
-    except:
-        pass
-    try:
+            redbiggest=-1
+            xcoor=int(x+(w/2))/wid
+    if len(contours2)>0:
         c2=max(contours2, key = cv2.contourArea)
         if cv2.contourArea(c2)>=minsize:
             cv2.drawContours(frame, c2, -1, (0,0,255), 3)
             x2,y2,w2,h2 =cv2.boundingRect(c2)
             cv2.rectangle(frame,(x2,y2),(x2+w2,y2+h2),(0,0,255),2)
             cv2.line(frame,(int(x2+(w2/2)),0),(int(x2+(w2/2)),len(blurred)),(0,0,255),1)
-    except:
-        pass
-    if cv2.contourArea(c2)>cv2.contourArea(c):
-        print("red closer",end="\r")
-    else:
-        print("green closer", end="\r")
+            redbiggest=1
+            xcoor = int(x2+(w2/2))/wid
+            if cv2.contourArea(c2)>cv2.contourArea(c):
+                print("red closer",end="\r")
+                redbiggest=1
+                xcoor = int(x2 + (w2 / 2))/wid
+            else:
+                print("green closer", end="\r")
+                redbiggest = -1
+                xcoor = int(x + (w / 2))/wid
+    if redbiggest!=0:
+        if redbiggest==1 :
+            pass # red is the bbiggest
+        if redbiggest==-1 :
+            pass # green is the biggest
 
-    
-
-            
-    
 
     cv2.imshow('Original', frame)
 
