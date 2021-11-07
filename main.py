b4f07e6fff4404e9cf31a00d5e33958cb6a96e50
DudeNav0J <jovan.angkaijie@gmail.com>
2021-11-07 11:38:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -204,6 +204,82 @@ upper_green = numpy.array([90, 255, 255])
 
 minsize=2000
 
+def checking_state():
+    print("moving state")
+    forward()
+    while (0):
+
+        redbiggest = 0
+        xcoor = None
+        _, frame = cap.read()
+        blurred = frame
+        print("Nothing.")
+
+        # Convert img to HSV
+        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+        green_mask = cv2.inRange(hsv, lower_green, upper_green)
+        red_mask = cv2.inRange(hsv, lower_red, upper_red) + cv2.inRange(hsv, lower_red2, upper_red2)
+        if wind == "YES":
+            cv2.imshow("redmask", red_mask)
+            cv2.imshow("greenmask", green_mask)
+
+        contours, _ = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+        contours2, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        # print("yes")
+        if len(contours) > 0:
+            # print("red")
+            c = max(contours, key=cv2.contourArea)
+            if cv2.contourArea(c) >= minsize:
+                cv2.drawContours(frame, c, -1, (0, 255, 0), 3)
+                x, y, w, h = cv2.boundingRect(c)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                cv2.line(frame, (int(x + (w / 2)), 0), (int(x + (w / 2)), len(blurred)), (0, 255, 0), 1)
+                redbiggest = -1
+                print("Green detected")
+                xcoor = int(x + (w / 2)) / wid
+        if len(contours2) > 0:
+            # print("green")
+            c2 = max(contours2, key=cv2.contourArea)
+            if cv2.contourArea(c2) >= minsize:
+                cv2.drawContours(frame, c2, -1, (0, 0, 255), 3)
+                x2, y2, w2, h2 = cv2.boundingRect(c2)
+                cv2.rectangle(frame, (x2, y2), (x2 + w2, y2 + h2), (0, 0, 255), 2)
+                cv2.line(frame, (int(x2 + (w2 / 2)), 0), (int(x2 + (w2 / 2)), len(blurred)), (0, 0, 255), 1)
+                redbiggest = 1
+                print("Red detected")
+                xcoor = int(x2 + (w2 / 2)) / wid
+                try:
+                    if cv2.contourArea(c2) > cv2.contourArea(c):
+                        print("red closer", end="\r")
+                        redbiggest = 1
+                        xcoor = int(x2 + (w2 / 2)) / wid
+                    else:
+                        print("green closer", end="\r")
+                        redbiggest = -1
+                        xcoor = int(x + (w / 2)) / wid
+                except:
+                    pass
+        if redbiggest != 0:
+            print("avoiding state")
+            avoidingstate(redbiggest)
+            pass
+        '''
+        if lightsensor sense floor thing
+        start turning right
+        until sense again then turn other direction
+        '''
+
+        if wind=="YES":
+            cv2.imshow('Original', frame)
+
+        k = cv2.waitKey(10) & 0xFF
+        if k == 27:
+            break
+        
+checking_state()
+
 def avoidingstate(redbiggest):
     if redbiggest==1:
         startturningtime = time.time()
@@ -334,83 +410,6 @@ def avoidingstate(redbiggest):
                     pass
                 stop()
 
-
-
-def checking_state():
-    print("moving state")
-    forward()
-    while (0):
-
-        redbiggest = 0
-        xcoor = None
-        _, frame = cap.read()
-        blurred = frame
-        print("Nothing.")
-
-        # Convert img to HSV
-        hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-
-        green_mask = cv2.inRange(hsv, lower_green, upper_green)
-        red_mask = cv2.inRange(hsv, lower_red, upper_red) + cv2.inRange(hsv, lower_red2, upper_red2)
-        if wind == "YES":
-            cv2.imshow("redmask", red_mask)
-            cv2.imshow("greenmask", green_mask)
-
-        contours, _ = cv2.findContours(green_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-        contours2, _ = cv2.findContours(red_mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-        # print("yes")
-        if len(contours) > 0:
-            # print("red")
-            c = max(contours, key=cv2.contourArea)
-            if cv2.contourArea(c) >= minsize:
-                cv2.drawContours(frame, c, -1, (0, 255, 0), 3)
-                x, y, w, h = cv2.boundingRect(c)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                cv2.line(frame, (int(x + (w / 2)), 0), (int(x + (w / 2)), len(blurred)), (0, 255, 0), 1)
-                redbiggest = -1
-                print("Green detected")
-                xcoor = int(x + (w / 2)) / wid
-        if len(contours2) > 0:
-            # print("green")
-            c2 = max(contours2, key=cv2.contourArea)
-            if cv2.contourArea(c2) >= minsize:
-                cv2.drawContours(frame, c2, -1, (0, 0, 255), 3)
-                x2, y2, w2, h2 = cv2.boundingRect(c2)
-                cv2.rectangle(frame, (x2, y2), (x2 + w2, y2 + h2), (0, 0, 255), 2)
-                cv2.line(frame, (int(x2 + (w2 / 2)), 0), (int(x2 + (w2 / 2)), len(blurred)), (0, 0, 255), 1)
-                redbiggest = 1
-                print("Red detected")
-                xcoor = int(x2 + (w2 / 2)) / wid
-                try:
-                    if cv2.contourArea(c2) > cv2.contourArea(c):
-                        print("red closer", end="\r")
-                        redbiggest = 1
-                        xcoor = int(x2 + (w2 / 2)) / wid
-                    else:
-                        print("green closer", end="\r")
-                        redbiggest = -1
-                        xcoor = int(x + (w / 2)) / wid
-                except:
-                    pass
-        if redbiggest != 0:
-            print("avoiding state")
-            avoidingstate(redbiggest)
-            pass
-        '''
-        if lightsensor sense floor thing
-        start turning right
-        until sense again then turn other direction
-        '''
-
-        if wind=="YES":
-            cv2.imshow('Original', frame)
-
-        k = cv2.waitKey(10) & 0xFF
-        if k == 27:
-            break
-        
-checking_state()
 
 
 
