ef20927e044dbc857fcf8357ca1614342c9cff59
DudeNav0J <jovan.angkaijie@gmail.com>
2021-11-08 13:24:50 +0800

    Update testt.py

--- testt.py
+++ testt.py
@@ -87,7 +87,7 @@ def movetank(s1,s2,tim):
 
 if __name__== "__main__":
     gos(forward, 1.4)
-    gos(right, 1.2)
+    gos(right, 1.3)
     gos(forward, 7.5)
     gos(right, 1.2)
     gos(forward, 8)
